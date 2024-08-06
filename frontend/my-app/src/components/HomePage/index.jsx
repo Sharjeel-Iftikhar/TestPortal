2fbcbb92f01d8f9efe81935f77486b64fa4a3d13
Sharjeel-Iftikhar <isharjeel63@gmail.com>
2024-08-07 00:29:06 +0500

    test-start-form

--- frontend/my-app/src/components/HomePage/index.jsx
+++ frontend/my-app/src/components/HomePage/index.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import Footer from '../Footer';
+import TestStartForm from '../TestStartForm';
 
 export const Home = () => {
   return (
@@ -33,8 +34,8 @@ export const Home = () => {
     </div>
 
     <div className='flex content-between items-stretch  gap-4'>
-      <div className='flex flex-col flex-grow items-stretch '>
-        <div className='test-card'> 
+      <div className='flex flex-col flex-grow items-stretch basis-[50%] '>
+        <div className='test-card basis-[100%]'> 
             <div className='text-[.75rem] leading-4 font-semibold uppercase text-[#0f2830] mt-[1px] ml-[4px]'>Honest Respondent Technology
             </div>
             <div className='flex mt-5 items-start'>
@@ -52,12 +53,12 @@ export const Home = () => {
                         
                 </div>
                 <div className='text-[1rem] leading-6 font-normal mt-[14px] mr-1 mb-2 ml-1'>
-                  <p>
+                  <p className='mt-4 mb-4'>
                   "The test is secured with "
                   <b>Honest Respondent Technology</b>
                   ". Don't click outside the test tab area. Every browser tab movement is recorded."
                   </p>
-                <p>
+                <p className='mt-4 mb-4'> 
                 We recommend disabling background programs, chats and system notifications before the test, as they can trigger a test block.
                 </p>
                 </div>
@@ -70,41 +71,8 @@ export const Home = () => {
       </div>
 
 
-      <div className='flex flex-col flex-grow items-stretch'>
-        <div className='test-card'> 
-            <div className='text-[.75rem] leading-4 font-semibold uppercase text-[#0f2830] mt-[1px] ml-[4px]'>Honest Respondent Technology
-            </div>
-            <div className='flex mt-5 items-start'>
-              <div className='pl-1 mr-3'>
-              <svg stroke="#0BC279" width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                            <path d="M10.49 2.23L5.50003 4.11C4.35003 4.54 3.41003 5.9 3.41003 7.12V14.55C3.41003 15.73 4.19003 17.28 5.14003 17.99L9.44003 21.2C10.85 22.26 13.17 22.26 14.58 21.2L18.88 17.99C19.83 17.28 20.61 15.73 20.61 14.55V7.12C20.61 5.89 19.67 4.53 18.52 4.1L13.53 2.23C12.68 1.92 11.32 1.92 10.49 2.23Z" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path>
-                            <path d="M9.05005 11.87L10.66 13.48L14.96 9.17999" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path>
-                        </svg>
-
-              </div>
-              <div className='flex-grow pr-1 pb-1'>
-                <div className='ml-1'>
-                  
-                Focus on your test only!
-                        
-                </div>
-                <div className='text-[1rem] leading-6 font-normal mt-[14px] mr-1 mb-2 ml-1'>
-                  <p>
-                  "The test is secured with "
-                  <b>Honest Respondent Technology</b>
-                  ". Don't click outside the test tab area. Every browser tab movement is recorded."
-                  </p>
-                <p>
-                We recommend disabling background programs, chats and system notifications before the test, as they can trigger a test block.
-                </p>
-                </div>
-
-              </div>
-
-            </div>
-        </div>
-
-      </div>
+      
+      <TestStartForm/>
 
     </div>
     <Footer/>
--- frontend/my-app/src/components/TestStartForm/index.jsx
+++ frontend/my-app/src/components/TestStartForm/index.jsx
@@ -1,31 +1,136 @@
-import React from 'react'
+import React from "react";
+import { useState } from "react";
 
 export default function TestStartForm() {
-  return (
-    <div className='flex flex-col flex-grow items-stretch'>
-        <div className='test-card'> 
-            <div className='text-[.75rem] leading-4 font-semibold tracking-wider uppercase text-[#0f2830] mt-[1px] ml-[4px]'>
-                
-            Test start form
-            
-            </div>
-            <div className='text-[1rem] font-normal leading-6 mt-[14px] mr-1 ml-1 mb-2'>
-                <div className='ml-0 mb-[14px]'>
-                    
-                Start the test
-                
-                </div>
-                <div className='text-[1rem] font-normal leading-6 mt-[14px] mr-1 ml-1 mb-2'>
-                    
-                    
-                    
-                Fill in the form before starting the test.
-                    
-                
-                </div>
+  const [formData, setFormData] = useState({
+    FirstName: "",
+    LastName: "",
+  });
+  const [isFocused, setIsFocused] = useState({ firstName: false, lastName: false });
 
-            </div>
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({ ...formData, [name]: value });
+  };
+
+  const handleFocus = (field) => {
+    setIsFocused({ ...isFocused, [field]: true });
+  };
+
+  const handleBlur = (field) => {
+    if (!formData[field]) {
+      setIsFocused({ ...isFocused, [field]: false });
+    }
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(formData);
+  };
+  return (
+    <div className="flex flex-col flex-grow items-stretch basis-[50%]">
+      <div className="test-card">
+        <div className="text-[.75rem] leading-4 font-semibold tracking-wider uppercase text-[#0f2830] mt-[1px] ml-[4px]">
+          Test start form
         </div>
+        {/* body div */}
+        <div className=" text-[1rem] font-normal leading-6 mt-[14px] mr-1 ml-1 mb-2">
+          <div className="ml-0 mb-[14px]">Start the test</div>
+          <div className="text-[1rem] font-normal leading-6 mt-[14px] mr-1 ml-1 mb-2">
+            Fill in the form before starting the test.
+          </div>
+
+
+          <form className="container" onSubmit={handleSubmit}>
+      <div className="w-full max-w-[460px] relative mt-8">
+        <input
+          type="text"
+          name="FirstName"
+          value={formData.FirstName}
+          onChange={handleChange}
+          onFocus={() => handleFocus('firstName')}
+          onBlur={() => handleBlur('firstName')}
+          required
+          className="w-full border-b-[1px] border-gray-400 focus:border-green-400 outline-none py-2 pl-0 pr-3 text-[1rem] leading-5 font-normal text-custom-dark transition duration-300"
+        />
+        <label
+          htmlFor="FirstName"
+          className={`absolute left-0 text-gray-500 transition-all duration-300 ${
+            isFocused.firstName || formData.FirstName ? 'top-[-20px] text-sm ' : 'top-2'
+          }`}
+        >
+          First name
+        </label>
+      </div>
+
+      <div className="w-full max-w-[460px] relative mt-10">
+        <input
+          type="text"
+          name="LastName"
+          value={formData.LastName}
+          onChange={handleChange}
+          onFocus={() => handleFocus('lastName')}
+          onBlur={() => handleBlur('lastName')}
+          required
+          className="w-full border-b-[1px] border-gray-400 focus:border-green-400 outline-none py-2 pl-0 pr-3 text-[1rem] leading-5 font-normal text-custom-dark transition duration-300"
+        />
+        <label
+          htmlFor="LastName"
+          className={`absolute left-0 text-gray-500 transition-all duration-300 ${
+            isFocused.lastName || formData.LastName ? 'top-[-20px] text-sm ' : 'top-2'
+          }`}
+        >
+          Last name
+        </label>
+      </div>
+
+      <button
+        type="submit"
+        className="mt-6 h-[40px] px-[18px] font-bold bg-custom-button-color text-white rounded hover:bg-green-600 transition duration-300"
+      >
+        Start test
+      </button>
+    </form>
+
+          {/* <form className="" onSubmit={handleSubmit}>
+            <div>
+              <label className="block text-sm leading-5 font-normal tracking-normal text-custom-dark mb-2">
+                First name
+              </label>
+              <input
+                type="text"
+                name="First name"
+                value={formData.FirstName}
+                onChange={handleChange}
+                required
+                className="w-full pt-3 pb-[14px] pl-[10px] pr-[10px]  h-[42px] overflow-visible px-3 py-2 border border-gray-300 rounded focus:outline-none focus:border-green-400 text-[1rem] leading-5 font-normal tracking-normal text-custom-dark"
+              />
+            </div>
+            <div>
+              <label className="block text-sm leading-5 font-normal tracking-normal text-custom-dark mb-2 mt-3">
+                Last name
+              </label>
+              <input
+                type="text"
+                name="Last name"
+                value={formData.LastName}
+                onChange={handleChange}
+                required
+                className="w-full pt-3 pb-[14px] pl-[10px] pr-[10px] mb-2  h-[42px] overflow-visible px-3 py-2 border border-gray-300 rounded focus:outline-none focus:border-green-400 text-[1rem] leading-5 font-normal tracking-normal text-custom-dark"
+              />
+            </div>
+
+            <button
+              type="submit"
+              className={`w-full ${
+                isSignUp ? "mt-[33px]" : "mt-[15px]"
+              } py-2 bg-custom-button-color text-white rounded hover:bg-green-600 transition duration-300`}
+            >
+              Start
+            </button>
+          </form> */}
+        </div>
+      </div>
     </div>
-  )
+  );
 }
